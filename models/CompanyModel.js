f97e9ee038f35a25438c8d0a2dcfd6e64c9c60e7
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-03-26 11:59:24 +0100

    added Company model

---
+++ models/CompanyModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const schema = new Schema(
+  {
+    companyName: String,
+    companyIndustry: String,
+    representativePhotoUrl: {
+      type: String,
+      default: "https://t4.ftcdn.net/jpg/04/10/43/77/360_F_410437733_hdq4Q3QOH9uwh0mcqAhRFzOKfrCR24Ta.jpg"
+    },
+    representativeFirstName: String,
+    representativeLastName: String,
+    representativePosition: String,
+    representativeEmail: String,
+    representativePassword: String,
+    companyLogoUrl: {
+      type: String,
+      default: "https://yoda.youthhubafrica.org/wp-content/uploads/2017/09/hkx40wm1lkzzqy35kptv.png"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const CompanyModel = mongoose.model("Company", schema);
+
+module.exports = CompanyModel;
