1c3044eba2d3ac410a917187c61b76676fdb02db
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-04-01 20:22:33 +0200

    2nd build push

--- app.js
+++ app.js
@@ -54,6 +54,13 @@ app.use("/api", applicationsRouter)
 app.use("/api", technologyRouter)
 app.use("/api", companyRouter)
 
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
+
 
 //404 Middleware
 app.use((req, res, next) => {
