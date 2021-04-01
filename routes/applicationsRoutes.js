07c93d6ee551e81414133deb5a1bc1c54f8f3fdc
Mohammed Adib <adibmohammed95@gmail.com>
2021-04-01 11:21:24 +0200

    Apps ObjectID Resolved

--- routes/applicationsRoutes.js
+++ routes/applicationsRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const AppsModel = require("../models/AppsModel")
 const TechnologyModel = require("../models/TechnologyModel")
+const CompanyModel = require("./../models/CompanyModel");
 const uploader = require("../config/cloudinary")
 const  protectRoute = require("./../middlewares/protectAuth.js")
 
@@ -43,11 +44,21 @@ router.get("/application/create",protectRoute, (req, res, next)=>{
 })
 // ensuite, on fait la requête pour créer la nouvelle application, tenant aussi compte des technologies existantes
 // Route Protection to prevent non-signed in users from accessing the App Creation Form
+// Remplacer Post par PATCH
 router.post("/application/create", uploader.single("appLogo"), (req, res, next)=>{ // PAS OUBLIER LE IS LOGGED IN
     let appLogo = req.file.path;
     const newApp = {...req.body, appLogo};
     AppsModel.create(newApp)
     .then((createdApp)=>{
+        console.log(`createdApp ::>>`, createdApp)
+        console.log(`I'm the session =>`, req.session.currentUser)
+        CompanyModel.findByIdAndUpdate(req.session.currentUser, {$push: {apps: createdApp._id}})
+        .then((dbRes) => {
+            console.log(dbRes)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
         res.status(200).json(createdApp)
     })
     .catch((error)=>{
@@ -59,12 +70,8 @@ router.post("/application/create", uploader.single("appLogo"), (req, res, next)=
 // ROUTE POUR UPDATER UNE APPLICATION
 // Route protection using protectAuth middleware to prevent non-signed in users from accessing the Apps Id and App form Update
 
-<<<<<<< HEAD
 router.patch("/applications/:id", uploader.single("picture"), (req, res, next)=>{
-=======
-router.patch("/applications/:id", uploader.single("appLogo"), (req, res, next)=>{
     // let appLogo = req.file.path
->>>>>>> f4cebd9edd6d915c9aaa6d1673abc016c12d72ea
     AppsModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
     .then((updatedApp)=>{
         res.status(200).json(updatedApp)
